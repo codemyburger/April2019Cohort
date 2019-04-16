d5a66b106763c48f8069e608ea24f9ceb00d193c
Veronica <veronica@digitalcrafts.com>
2019-04-16 14:46:09 -0500

    redirectin on command line and python 101

---
+++ Week1/Tuesday/class.py
@@ -0,0 +1,145 @@
+
+
+# print('hello world')
+
+# print('one', 'two', 'three')
+
+# print('One'),
+# print('Two'),
+# print('Three')
+
+# import sys
+
+# sys.stdout.write('hello world\nhello\tworld\n')
+
+# name = "Veronica"
+
+# print(name)
+
+# found_coins = 15
+
+# magic_coins = 20
+
+# result = found_coins + magic_coins
+
+# print(result)
+
+# first_name = "Veronica"
+# last_name = "Lino"
+
+# name = first_name + " " +  last_name
+
+# print(name)
+
+# name = "Veronica"
+# age = 31
+
+# sentence = "%s is %d years old %s"%(name, age, name)
+
+# print(sentence)
+
+first_name = "Veronica"
+last_name = "Lino"
+
+# sentence = "Hello {} {}".format(first_name, last_name)
+
+# print(sentence)
+
+# sentence = "Hello {0} {1} My last name is {1}".format(first_name, last_name)
+
+
+# sentence = """
+# sldjslkdf {first}
+# sldkfjslkdf
+# """.format(first=first_name, last=last_name)
+# print(sentence)
+
+# print("\a")
+
+# a = 3
+
+# b = 3/4
+
+# c = a + b
+
+# print(c )
+
+# myNumber = '45'
+
+# myNumber = int(myNumber)
+
+# c = myNumber + 1 
+
+# word = int('blah')
+# print(word)
+
+# birthYear = input('what is your year of birth >> ')
+
+# age = 2019 - int(birthYear)
+
+# sentence = "The user's name is: {}".format(name)
+
+# print(age)
+
+# if (age >= 21):
+#     print('You get free beer')
+
+# if age >= 21:
+#     print('{} you get free beer'.format(age))
+# else:
+#     print('{} Sorry no beer for you'.format(age))
+
+
+# people = 20
+# cats = 30
+# dogs = 15
+
+# dogs = dogs + 5
+# dogs += 5
+
+# if people < cats:
+#     print("Too many cats! The world is doomed!")
+
+# if people >= dogs:
+#     print("People are greater than or equal to dogs.")
+
+# if people <= dogs:
+#     print("People are less than or equal to dogs.")
+
+
+# if people == dogs:
+#     print("People are dogs.")
+
+
+# people = 300
+# cars = 40
+# trucks = 15
+
+# if cars > people:
+#     print("We should take the cars.")
+# elif cars == people:
+#     print("We should not take the cars.")
+# else:
+#     print("We can't decide.")
+
+# count = 0
+
+# while (count < 10):
+#     # count = count + 1
+#     count += 1
+#     print("The count is", count)
+
+# answer = ''
+
+# while (answer != 'when'):
+#     answer = input('say when>> ')
+#     answer = answer.lower()
+
+# while True:
+#   answer = input('Say when: ')
+#   if answer == 'When':
+#       if answer.lower() == 'when':
+#         break
+# print("Cheese")
+
+# print('cheese')
