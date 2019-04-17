da7e7e6e29bfa7ef40df8f9d5a6362545101dece
Veronica <veronica@digitalcrafts.com>
2019-04-17 12:58:19 -0500

    sequences, loops

---
+++ Week1/Wednesday/class.py
@@ -0,0 +1,153 @@
+
+
+# a = "1"
+
+# myList = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+
+# s = myList[0:2]
+
+# print(s)
+
+# myList[1] = "saturday"
+# print(myList[1])
+
+# num = len(myList)
+
+# print(num)
+
+# myNum = [1, "tuesday", False, 4, 5, 6]
+
+# print(myNum)
+
+# myNum.append(True)
+
+# print(myNum)
+
+
+# num = [1, 2, 3, 4, 5, 6, 7]
+
+# s = num[4:5]
+
+# print(s)
+
+# num = [3,4,5]
+
+# num.extend([1,2,3])
+# print(num)
+
+# num.pop()
+
+# print(num)
+
+# a = num.index(1)
+
+# print(a)
+
+# a = num.sort()
+# print(a)
+
+# a = [1, 2, 4]
+
+# b = a.copy()
+
+# b[1] = 3
+
+# print(a)
+# print(b)
+# a = 1
+# b = a 
+
+# print(a)
+# print(b)
+# b = 3
+# print(b) 
+
+# a = [1, 2, 3]
+
+# print(a)
+
+# a.clear()
+
+# print(a)
+
+
+# myString = "hello world"
+
+# # print(myString[0])
+
+# print(myString[1:4])
+
+# a = [1, 2, 4] * 3
+# b = [5, 6, 7]
+
+# print("a"* 3)
+
+# multi = [[1, 3], [2,4], [4, 5], [5, 6]]
+
+# print(multi[1][1])
+
+# myTuple = (45, 23, 12, 67)
+
+# print(myTuple)
+
+# myTuple(2) = 45
+
+# one, two, something = (1, 2, 3)
+
+# print(something)
+
+# a = 1
+# b = 2
+
+# c = (a, b)
+
+# a = 3
+# print(a)
+# print(c)
+
+
+# names = ["George", "W", "Bush"]
+
+# for name in names:
+#     print(name)
+
+# name = "Houston Metro Area"
+
+# for char in name:
+#     print(char)
+
+
+# for num in range(10):
+#     print(num)
+#     for inner in range(10):
+#         print(inner)
+
+# for outer in range(1,11):
+#     for inner in range(1,11):
+#         result = outer * inner
+#         # print("{0}x{1}={2}".format(outer, inner, result))
+#         print(f"{outer}x{inner}={result}")
+
+a = 1
+b = 2
+c = 3
+
+if (a == 1):
+    print('do this')
+
+if (b  == 2):
+    print(b)
+
+if (c == 3):
+    print(c)
+
+if (a == 1):
+    print('do this')
+elif (b  == 2):
+    print(b)
+elif (c == 3):
+    print(c)
+
+
+    
+
